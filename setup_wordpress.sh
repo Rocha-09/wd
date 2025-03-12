ca25e36d052baae1b0dc9954e2ba40e4d4a99254
Rocha-09 <rochaness@sookmyung.ac.kr>
2025-03-12 20:45:07 +0800

    Update setup_wordpress.sh

--- setup_wordpress.sh
+++ setup_wordpress.sh
@@ -1,1 +1,86 @@
+#!/bin/bash
 
+# 设置域名和邮箱
+DOMAIN="defenseconsulting.me"
+EMAIL="ericshum2025@outlook.com"
+
+# 创建docker-compose.yml文件
+cat <<EOF > docker-compose.yml
+version: '3.7'
+
+services:
+  wordpress:
+    image: wordpress:latest
+    depends_on:
+      - db
+    ports:
+      - "80:80"
+      - "443:443"
+    environment:
+      WORDPRESS_DB_HOST: db:3306
+      WORDPRESS_DB_USER: wordpress
+      WORDPRESS_DB_PASSWORD: wordpress
+      WORDPRESS_DB_NAME: wordpress
+    volumes:
+      - wordpress_data:/var/www/html
+      - ./letsencrypt:/etc/letsencrypt
+      - ./nginx.conf:/etc/nginx/conf.d/default.conf
+
+  db:
+    image: mysql:5.7
+    environment:
+      MYSQL_DATABASE: wordpress
+      MYSQL_USER: wordpress
+      MYSQL_PASSWORD: wordpress
+      MYSQL_ROOT_PASSWORD: root_password
+    volumes:
+      - db_data:/var/lib/mysql
+
+  certbot:
+    image: certbot/certbot
+    volumes:
+      - ./letsencrypt:/etc/letsencrypt
+      - ./certbot/www:/var/www/certbot
+
+volumes:
+  wordpress_data:
+  db_data:
+
+networks:
+  default:
+    driver: bridge
+EOF
+
+# 创建并启动Docker服务
+docker-compose up -d && \
+sleep 30 && \
+docker-compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot -d $DOMAIN --email $EMAIL --agree-tos --no-eff-email --staging && \
+mkdir -p ./letsencrypt/live/$DOMAIN && \
+cat <<EOF > ./nginx.conf
+server {
+    listen 80;
+    server_name $DOMAIN;
+
+    location / {
+        return 301 https://\$host\$request_uri;
+    }
+}
+
+server {
+    listen 443 ssl;
+    server_name $DOMAIN;
+
+    ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
+
+    location / {
+        proxy_pass http://wordpress:80;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+EOF
+docker-compose exec wordpress nginx -s reload && \
+echo "WordPress 已成功设置并运行在 https://$DOMAIN"
